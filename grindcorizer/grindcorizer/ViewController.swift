63dbc9e59c48bd4f97a736e91e62365825575d43
Linus Åkerlund <linus.akerlund@bontouch.com>
2018-03-19 21:56:24 +0100

    Added Xcode project.

---
+++ grindcorizer/grindcorizer/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  grindcorizer
+//
+//  Created by Linus Akerlund on 2018-03-19.
+//  Copyright © 2018 Puterman. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ grindcorizer/grindcorizerTests/grindcorizerTests.swift
@@ -0,0 +1,36 @@
+//
+//  grindcorizerTests.swift
+//  grindcorizerTests
+//
+//  Created by Linus Akerlund on 2018-03-19.
+//  Copyright © 2018 Puterman. All rights reserved.
+//
+
+import XCTest
+@testable import grindcorizer
+
+class grindcorizerTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
