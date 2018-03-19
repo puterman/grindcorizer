b831355fb8db163b506ca1a92180724764e18c69
Linus Åkerlund <linus.akerlund@bontouch.com>
2018-03-19 22:21:27 +0100

    Added image picker controller to record video.

--- grindcorizer/grindcorizer/ViewController.swift
+++ grindcorizer/grindcorizer/ViewController.swift
@@ -3,13 +3,36 @@
 //
 
 import UIKit
+import MobileCoreServices
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UINavigationControllerDelegate {
 
     @IBAction func recordVideoTapped() {
-        print("Record video")
+        launchVideoRecorder()
     }
 
 
+    private func launchVideoRecorder() {
+        if UIImagePickerController.isSourceTypeAvailable(.camera) == false {
+            print("Camera not available...")
+        }
+
+        let cameraController = UIImagePickerController()
+        cameraController.sourceType = .camera
+        cameraController.mediaTypes = [kUTTypeMovie as String]
+        cameraController.allowsEditing = false
+        cameraController.delegate = self
+
+        present(cameraController, animated: true, completion: nil)
+    }
 }
 
+extension ViewController: UIImagePickerControllerDelegate {
+    func imagePickerControllerDidCancel(_ picker: UIImagePickerController) {
+
+    }
+
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [String : Any]) {
+
+    }
+}
