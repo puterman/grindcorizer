e4744ba60528b4237b247355d4e221024732b057
Linus Åkerlund <linus.akerlund@bontouch.com>
2018-03-19 22:29:00 +0100

    Dismiss image picker on cancel or successful capture.

--- grindcorizer/grindcorizer/ViewController.swift
+++ grindcorizer/grindcorizer/ViewController.swift
@@ -17,6 +17,8 @@ class ViewController: UIViewController, UINavigationControllerDelegate {
             print("Camera not available...")
         }
 
+        // FIXME: available media types etc.
+
         let cameraController = UIImagePickerController()
         cameraController.sourceType = .camera
         cameraController.mediaTypes = [kUTTypeMovie as String]
@@ -29,10 +31,13 @@ class ViewController: UIViewController, UINavigationControllerDelegate {
 
 extension ViewController: UIImagePickerControllerDelegate {
     func imagePickerControllerDidCancel(_ picker: UIImagePickerController) {
-
+        dismiss(animated: true, completion: nil)
     }
 
     func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [String : Any]) {
+        let path = info["UIImagePickerControllerMediaURL"]
+        print("Finished picking media with file: \(path)")
 
+        dismiss(animated: true, completion: nil)
     }
 }
