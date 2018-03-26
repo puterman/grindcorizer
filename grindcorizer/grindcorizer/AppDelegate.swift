9915ef455af215f0b210f8bf6bf2dbc4e92ef2ee
Linus Åkerlund <linus.akerlund@bontouch.com>
2018-03-26 20:34:57 +0200

    Initialize random number generator.

--- grindcorizer/grindcorizer/AppDelegate.swift
+++ grindcorizer/grindcorizer/AppDelegate.swift
@@ -14,6 +14,9 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     var window: UIWindow?
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
+
+        srand48(Int(time(nil)))
+
         return true
     }
 }
